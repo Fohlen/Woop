604331acfb09e2deccc87b6eb67fadf0d51fa87b
Fohlen <fohlen@einhufer.info>
2014-11-19 20:26:27 +0100

    Tiny <span> hack to get the description with white color

--- template.php
+++ template.php
@@ -51,7 +51,7 @@ class woopsquadtemplate extends basesquadtemplate
 		
 		foreach($this->squad->members as $member) {
 			
-			$html[] = '<h2><a name="'.$member->membername.'" href="#'.$member->membername.'">'.$member->membername.' style="color: #fff;"</a></h2>';
+			$html[] = '<h2><a name="'.$member->membername.'" href="#'.$member->membername.'">'.$member->membername.'</a></h2>';
 			$html[] = '<dl class="p_stats">';
 			$html[] = '<dd class="dim"><img src="'.IntegrationHelper::getFullAvatarImagePath($member->avatar).'" alt="'.$member->membername.'" style="border-radius: 10px;"></dd>';
 			/*foreach ($this->fieldlist as $field) 
@@ -63,7 +63,12 @@ class woopsquadtemplate extends basesquadtemplate
 			
 			$html[] = '<dd>'.$this->getLastSquadMemberOnline($member).'</dd>';
 			$html[] = '</dl>';
-			if ($member->description != '') $html[] = $member->description;
+			if ($member->description != '')
+			{
+				$html[] = '<span style="color: #fff !important">';
+				$html[] = $member->description;
+				$html[] = '</span>';
+			}
 			
 			$html[] = '<hr style="background-color: #747474;">'; // Separate the profiles though
 		}
